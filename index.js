2a4b64baae564e266f8cf67ad88197ce8ab81c8e
TYMOFII HRYHORIEV <96590888+Timajeem@users.noreply.github.com>
2022-01-09 15:43:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log("Hello");
+let foo=0;
+switch(foo){
+	case -1:
+	console.log("negative1");
+	break;
+case 0:
+	console.log(0);
+	break;
+case 1:
+	console.log(1);
+	break;
+case 2:
+	console.log(2);
+	break;
+	default:
+	console.log("errrrro");
+}
