d07ea1445009b158ef5541f3f77c5239c96cc22b
TYMOFII HRYHORIEV <96590888+Timajeem@users.noreply.github.com>
2022-01-13 14:28:01 +0200

    Add files via upload

---
+++ homework 1/index.js
@@ -0,0 +1,70 @@
+// // Task2:
+// console.log(typeof 9);
+// console.log(typeof strik);
+// console.log(typeof "strik");
+// console.log(typeof true);
+// console.log(typeof null);
+// console.log(typeof 1 / 0);
+
+// // Task3
+// let name =2
+// if(typeof name =='string'){
+// 	console.log("hellow",name);
+// }else{
+// 	console.log("Errrrrorrr")
+// }
+
+// // Tast4
+// var firstCheck = 9,
+//     secondCheck = true,
+//     access = firstCheck ? "number" : secondCheck ? "undefined" : "111";
+
+// console.log( access );//тут я запутался
+
+// // Task5
+// let Day = "45765";
+// switch (Day){
+// case "Saturday":
+// case "Sunday":
+// console.log("Its a day off");
+// break;
+// case "Monday":
+// case "Tuesday":
+// case "Wednesday":
+// case "Thursday":
+// case "Friday":
+// console.log("This is a working day");
+// break;
+// default:
+// console.log("Enter the day of the week");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
