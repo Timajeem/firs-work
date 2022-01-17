01e0da63120ac5fcdd787dbfb2ad392aa7842bcc
TYMOFII HRYHORIEV <96590888+Timajeem@users.noreply.github.com>
2022-01-17 14:03:59 +0200

    Add files via upload

---
+++ homework 3/index.js
@@ -0,0 +1,160 @@
+// Task1
+// var x = Number(prompt("Введите целое цисло>0"));
+// function prov()
+// {
+//     if(x<=0 || isNaN(x))
+//     {
+//         x = Number(prompt("Введите целое цисло>0"));
+//         prov();
+//     }else
+//     {
+//         var sum=2;
+//         for (var i = 1; i <= x; i++) 
+//         {
+//             sum= sum * i;
+//         }
+//         alert(sum);
+//     }
+// }
+// prov();
+// Task2
+// function average(nums) {
+//     return nums.reduce((a, b) => (a + b)) / nums.length;
+//     console.log(a,b);
+// }
+
+// Task3
+// let string = '1,6,3,б,10,15,13,10';
+// let numbers = string.split('');
+// let sum = 0;
+// numbers.forEach(number => {if(number % 2 === 0) sum += +number});
+// console.log(sum);
+
+// Task5
+// var ok = confirm('Разрешаете ли вы использование cookie?');
+// alert(ok);
+
+// Task6
+// var arr = [3,2,5,6,7,8];
+// function Sum(array){
+// var sum = 0;
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i];
+//     }
+// console.log(sum);
+// }
+// arraySum(arr);
+// var myarray=["Bob", "Bully", "Amy"]
+// myarray.sort()
+// console.log(myarray);
+
+// Task7
+// moment().format('YYYY MM DD');
+
+// var dateEntered = $('#txtEnteredDate').val();
+
+ 
+
+// if (!moment(dateEntered,'MM-DD-YYYY').isValid()) {
+
+//   console.log('Invalid Date');
+
+// } else {
+
+//   console.log('Valid Date');
+
+// }
+// Task4
+// function User(name) {
+//   this.name = name;
+// }
+
+// let user = new User("Вася");
+
+// alert(user.name);
+
+// let user = {
+//   name: "John",
+//   age: 30
+// };
+
+// for (let value of Object.values(user)) {
+//   alert(value); 
+// }
+
+// function sum(sal) {
+
+//   let sum = 0;
+//   for (let salary of Object.values(sal)) {
+//     sum += salary;
+//   }
+
+//   return sum;
+// }
+
+// let sal = {
+//   "John": 150,
+//   "Pete": 700,
+//   "Mary": 250
+// };
+
+// alert( sum(sal) );
+
+// Task9
+// на index.html сделал что-то подобное
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
